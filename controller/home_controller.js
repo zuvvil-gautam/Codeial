1a968884233b95e36de5effe8e58c317bc1d6e3e
zuvvil <zuvvilgautam007@gmail.com>
2023-07-14 12:51:20 +0530

    add a controller and a router for home

---
+++ controller/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function(req,res){
+    res.send('<h1>Hello Zuvvil, your express server is up for social app</h1>');
+}
+
+//module.exports.actionName = function(req,res){}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const homeController = require('../controller/home_controller');
 
 console.log('router loaded')
+
+router.get('/', homeController.home)
 
 module.exports = router;
