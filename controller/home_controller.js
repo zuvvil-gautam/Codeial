5ed1d19c196b8919a7785877c02dd7e2ee14ab9c
zuvvil <zuvvilgautam007@gmail.com>
2023-07-15 10:52:54 +0530

    render homepage using ejs as view engine

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req,res){
-    res.send('<h1>Hello Zuvvil, your express server is up for social app</h1>');
+
+    res.render('home',{
+        title: 'Home' //title of the page to be displayed in browser tab and header section
+    });
 }
 
 //module.exports.actionName = function(req,res){}
