21a335997c594238d18f622cf7df12739a2d4686
zuvvil <zuvvilgautam007@gmail.com>
2023-07-21 08:43:40 +0530

    create layout for the application

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 //use express router
 app.use('/', require('./routes'));
 
