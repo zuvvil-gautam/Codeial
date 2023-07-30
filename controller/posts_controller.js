0d2516851ba7cb16c595f5d51f0af2f5a3fb0158
zuvvil <zuvvilgautam007@gmail.com>
2023-07-30 14:05:16 +0530

    create a controller and router to delete a post and respective comments

--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function (req, res) {
     Post.create({
@@ -15,3 +16,37 @@ module.exports.create = function (req, res) {
             res.redirect('back');
         });
 };
+
+// Function to find a post by ID and return a promise
+function findByIdAsync(id) {
+    return Post.findById(id).lean().exec();
+}
+
+// Function to delete comments by post ID and return a promise
+function deleteCommentsByPostIdAsync(postId) {
+    return Comment.deleteMany({ post: postId }).exec();
+}
+
+module.exports.destroy = function (req, res) {
+    findByIdAsync(req.params.id)
+        .then(post => {
+            if (!post) {
+                return Promise.reject('Post not found');
+            }
+
+            if (post.user.toString() === req.user.id) {
+                return deleteCommentsByPostIdAsync(req.params.id)
+                    .then(() => Post.findByIdAndDelete(req.params.id).exec());
+            } else {
+                return Promise.reject('User is not authorized to delete this post');
+            }
+        })
+        .then(() => {
+            res.redirect('back');
+        })
+        .catch(err => {
+            console.error('Error in deleting post and comments:', err);
+            res.status(500).send('Error deleting post and comments. Please try again later.');
+        });
+};
+
