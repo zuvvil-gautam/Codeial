bba6fb5f60d2056483f4e95ac4809fb6e358dfee
zuvvil <zuvvilgautam007@gmail.com>
2023-07-31 11:03:29 +0530

    distribute post and comment ejs code into partials

--- routes/comments.js
+++ routes/comments.js
@@ -8,4 +8,4 @@ router.post('/create', passport.checkAuthentication, commentsController.create);
 router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 
-module.exports = router;
+module.exports = router; 
