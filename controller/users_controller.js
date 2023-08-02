5218069fc571a439ceeebd6b3c939a19092c41ae
zuvvil <zuvvilgautam007@gmail.com>
2023-08-02 20:35:18 +0530

    Fixed Profile link issue

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -37,19 +37,19 @@ module.exports.update = function (req, res) {
 //render the sign in page
 module.exports.signIn = function(req,res){
     if (req.isAuthenticated()) {
-        return res.redirect('/user/profile');
+        res.redirect('/user/profile/' + req.user.id);
     }
 
-    res.render('user_sign_in',{
+    return res.render('user_sign_in',{
         title: "Codial | Sign In"
     })
 }
 //render the sign up page
 module.exports.signUp = function(req,res){
     if(req.isAuthenticated()){
-        return res.redirect('/user/profile');
+        res.redirect('/user/profile/' + req.user.id);
     }
-    res.render('user_sign_up',{
+    return res.render('user_sign_up',{
         title: "Codial | Sign Up"
     })
 }
@@ -88,7 +88,7 @@ module.exports.create = function (req, res) {
 
 
 module.exports.createSession = function(req,res){
-    res.redirect('/user/profile');
+    res.redirect('/user/profile/' + req.user.id);
 }
 
 module.exports.destroySession = function (req, res) {
