35b53d89e0116601942f58c0b5f2aedb938da883
zuvvil <zuvvilgautam007@gmail.com>
2023-08-03 11:30:15 +0530

    Converting more Code to async/await

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -1,80 +1,58 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function (req, res) {
-    Post.findById(req.body.post)
-        .then(post => {
-            if (!post) {
-                console.log('Post not found');
-                return res.redirect('/');
-            }
+module.exports.create = async function (req, res) {
+    try{
 
-            Comment.create({
+        let post =await Post.findById(req.body.post);
+        
+        if (!post) {
+            console.log('Post not found');
+            res.redirect('/');
+        }
+        
+        if (post) {
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            })
-                .then(comment => {
-                    post.comments.push(comment);
-                    return post.save();
-                })
-                .then(() => {
-                    res.redirect('/');
-                })
-                .catch(err => {
-                    console.log('Error in posting comment:', err);
-                    res.redirect('/');
-                });
-        })
-        .catch(err => {
-            console.log('Error finding post:', err);
+            });
+            
+            post.comments.push(comment);
+            post.save();
+            
             res.redirect('/');
-        });
+        }
+    } 
+                
+    catch(err){
+        console.log('Error in posting comment:', err);
+        res.redirect('/');
+    }
+    
+        
 };
 
 
+module.exports.destroy = async function(req,res){
 
-// Function to find a comment by ID and return a promise
-function findCommentByIdAsync(id) {
-    return Comment.findById(id).exec();
-}
+    try {
+        let comment = await Comment.findById(req.params.id);
 
-// Function to update a post and return a promise
-function updatePostAsync(postId, commentId) {
-    return Post.findByIdAndUpdate(postId, { $pull: { comments: commentId } }).exec();
-}
+        if(comment.user == req.user.id){
 
-module.exports.destroy = function (req, res) {
-    // Step 1: Find the comment by its ID
-    findCommentByIdAsync(req.params.id)
-        .then((comment) => {
-            // Step 2: Check if the comment exists
-            if (!comment) {
-                return Promise.reject('Comment not found');
-            }
+            let postId = comment.post;
+            comment.deleteOne();
 
-            // Step 3: Check if the current user is the owner of the comment
-            if (comment.user.toString() === req.user.id) {
-                // Step 4: Delete the comment and update the associated post
-                let postId = comment.post;
+            let post = Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id} });
 
-                // Step 4.1: Delete the comment
-                return comment.deleteOne().then(() => {
-                    // Step 4.2: Update the post by removing the comment ID from the comments array
-                    return updatePostAsync(postId, req.params.id);
-                });
-            } else {
-                return Promise.reject('User is not authorized to delete this comment');
-            }
-        })
-        .then(() => {
-            // Step 5: Redirect the user back to the previous page after successful deletion
             res.redirect('back');
-        })
-        .catch((err) => {
-            // Step 6: Handle errors if any
-            console.error('Error in deleting comment:', err);
-            res.status(500).send('Error deleting comment. Please try again later.');
-        });
-};
-
+ 
+        } else{
+            res.redirect('back');
+        }
+    } catch(err){
+        console.log('Error',err);
+        return;
+    }
+}
--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -17,36 +17,25 @@ module.exports.create = function (req, res) {
         });
 };
 
-// Function to find a post by ID and return a promise
-function findByIdAsync(id) {
-    return Post.findById(id).lean().exec();
-}
+module.exports.destroy = async function (req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
 
-// Function to delete comments by post ID and return a promise
-function deleteCommentsByPostIdAsync(postId) {
-    return Comment.deleteMany({ post: postId }).exec();
-}
+        if (!post) {
+            return res.status(404).send('Post not found');
+        }
 
-module.exports.destroy = function (req, res) {
-    findByIdAsync(req.params.id)
-        .then(post => {
-            if (!post) {
-                return Promise.reject('Post not found');
-            }
-
-            if (post.user.toString() === req.user.id) {
-                return deleteCommentsByPostIdAsync(req.params.id)
-                    .then(() => Post.findByIdAndDelete(req.params.id).exec());
-            } else {
-                return Promise.reject('User is not authorized to delete this post');
-            }
-        })
-        .then(() => {
-            res.redirect('back');
-        })
-        .catch(err => {
-            console.error('Error in deleting post and comments:', err);
-            res.status(500).send('Error deleting post and comments. Please try again later.');
-        });
+        if (post.user == req.user.id) {
+            await post.deleteOne();
+            await Comment.deleteMany({ post: req.params.id });
+            return res.redirect('back');
+        } else {
+            return res.status(401).send('Unauthorized');
+        }
+    } catch (err) {
+        console.error('Error in deleting post and comments:', err);
+        return res.status(500).send('Error deleting post and comments. Please try again later.');
+    }
 };
+
 
