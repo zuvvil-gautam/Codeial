8486f8f4ba766bf7c523c052070edde2c3fb36af
zuvvil <zuvvilgautam007@gmail.com>
2023-08-03 10:41:47 +0530

    Converting promises to async await

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -2,32 +2,34 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 
-module.exports.home = function (req, res) {
-    // Populate the user of each post and its comments using promises
-    Post.find({})
+module.exports.home = async function (req, res) {
+
+    try{
+
+        // Populate the user of each post and its comments using promises
+        let posts = await Post.find({})
         .populate('user')
         .populate({
             path: 'comments',
             populate: {
                 path: 'user'
             }
-        })
-        .exec()
-        .then(posts => {
-            // Fetch all users using a promise
-            return User.find({}).exec().then(users => {
-                // Render the home view with the retrieved data
-                return res.render('home', {
-                    title: "Codeial | Home",
-                    posts: posts,
-                    all_users: users
-                });
-            });
-        })
-        .catch(err => {
-            console.log("Error:", err);
-            return res.status(500).send("Internal Server Error");
         });
+        
+        // Fetch all users using a promise
+        let users = await User.find({});
+        
+        
+        // Render the home view with the retrieved data
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
+        });
+    } catch(err){
+        console.log("Error:", err);
+        return res.status(500).send("Internal Server Error");
+    }
 };
 
 
