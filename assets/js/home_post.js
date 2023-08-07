9a5f2455e43ea2379b6defdbd797206980e13644
zuvvil <zuvvilgautam007@gmail.com>
2023-08-07 18:07:31 +0530

    Convert Posts to AJAX

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -15,6 +15,15 @@
                     $('#posts-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button', newPost));
 
+                    new Noty
+                    ({
+                        theme: 'relax',
+                        text: 'Post published',
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                    }).show();
+
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -69,6 +78,15 @@
                 success : function(data)
                 {
                     $(`#post-${data.data.post_id}`).remove();
+
+                    new Noty
+                    ({
+                        theme: 'relax',
+                        text: 'Post Deleted',
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                    }).show();
                 },
                 error : function(error)
                 {
@@ -77,5 +95,20 @@
             });
         });
     }
+
+      // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+      let convertPostsToAjax = function()
+      {
+        $('#posts-list-container>ul>li').each(function(){
+            let self = $(this);
+            let deleteButton = $(' .delete-post-button', self);
+            deletePost(deleteButton);
+
+            // get the post's id by splitting the id attribute
+
+            let postId = self.prop('id').split("-")[1];
+        })
+      }
     createPost();
+    convertPostsToAjax();
 }
