eb53a6ca605ea9e92d96c91cc3f3312e350843fb
zuvvil <zuvvilgautam007@gmail.com>
2023-08-21 18:31:01 +0530

    Playing with APIs

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const postsApi = require('../../../controller/api/v1/posts_api');
 
 router.get('/', postsApi.index);
+router.delete('/:id', postsApi.destroy);
 
 module.exports = router;
