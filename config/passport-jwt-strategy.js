69d521b870b00b361ca9c26ffe43412a91a7954f
zuvvil <zuvvilgautam007@gmail.com>
2023-08-23 11:06:06 +0530

    Authentication and Authorization

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -5,21 +5,24 @@ const ExtractJWT = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 
 let opts = {
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'codeial'
 }
 
 passport.use(new JWTStrategy(opts, function(jwtPayload,done){
 
-    User.findById(jwtPayload._id, function(err,user){
-        if(err){console.log('Error in finding user from JWT'); return;}
-
-        if(user){
-            return done(null,user);
-        }else{
-            return done(null,false);
-        }
-    })
+    User.findById(jwtPayload._id)
+        .then(user => {
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+        .catch(err => {
+            console.log('Error in finding user from JWT', err);
+            return done(err, false);
+        });
 }));
 
 module.exports = passport;
