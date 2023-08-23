9e79c9466ddd3ab852d9f2ba60f9a0019b35d96f
zuvvil <zuvvilgautam007@gmail.com>
2023-08-23 23:28:00 +0530

    Using Google Auth :: With a Link

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -4,33 +4,34 @@ const crypto = require('crypto');
 const User = require('../models/user');
 
 //tell passport to use a new strategy for google login
-passport.user(new googleStrategy({
+passport.use(new googleStrategy({
     clientID: "664345242393-ddkvpa42u0ccd60t5g7lag0bv9u3ggqt.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: "http://localhost:8000/users/auth/google/callback",
+    callbackURL: "http://localhost:8000/user/auth/google/callback",
 },
 
-function(accessToken, refreshToken, profile, done){
-    //find a user
-    User.findOne({email: profile.emails[0].value}).exec(function(err,user){
-        console.log(profile);
+    async function (accessToken, refreshToken, profile, done) {
+        try {
+            let user = await User.findOne({ email: profile.emails[0].value }).exec();
 
-        if(user){
-            //if found, set this user as req.user
-            return done(null,user);
-        }else{
-            //if not found, create the user and set it as req.user
-            User.create({
-                name: profile.displayName,
-                email: profile.emails[0].value,
-                password: crypto.randomBytes[20].toString('hex')
-            },function(err,user){
-                if (err) {console.log('error in creating user google strategy-passport',err); return; }
-                return done(null,user);
-            })
+            if (user) {
+                // If user exists, set this user as req.user
+                return done(null, user);
+            } else {
+                // If user doesn't exist, create the user and set it as req.user
+                let newUser = await User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                });
+                return done(null, newUser);
+            }
+        } catch (err) {
+            console.log('error in creating user google strategy-passport', err);
+            return done(err);
         }
-    })
-}
+    }
+
 
 
 ));
