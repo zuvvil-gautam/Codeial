9f807c08413e8ce75d0e93d93d39abc2f1f23dba
zuvvil <zuvvilgautam007@gmail.com>
2023-08-23 08:13:11 +0530

    Setting up Passport JWT

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
