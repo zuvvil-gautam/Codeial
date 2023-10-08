02f051d20294d1acfda030e0ad043f15678e5a7c
zuvvil <zuvvilgautam007@gmail.com>
2023-10-08 12:19:56 +0530

    Send HTML Template Emails

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -23,7 +23,7 @@ let renderTemplate = function(data, relativePath){
         function(err,template)
         {
             if(err){
-                console.log('Error in rendering template');
+                console.log('Error in rendering template',err);
                 return;
             }
             mailHTML = template;
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,33 +2,16 @@ const nodemailer = require('../config/nodemailer');
 
 // This is another way of exporting a method
 exports.newComment = (comment) => {
-    console.log('Inside newComment mailer', comment);
 
-    // Check if comment is defined
-    if (!comment) {
-        console.log('Comment is undefined or missing.');
-        return;
-    }
-
-    // Check if comment.user is defined
-    if (!comment.user) {
-        console.log('User is undefined or missing within the comment.');
-        return;
-    }
-    // Check if comment.user.email is defined
-
-    if (!comment.user.email) {
-        console.log('User email is undefined or missing within the comment.');
-        return;
-    }
-
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+  
     // If all checks pass, send the email
     nodemailer.transporter.sendMail(
         {
             from: 'zuvvilgautamr123@gmail.com',
             to: comment.user.email,
             subject: "New Comment Published!",
-            html: '<h1>Yup, your comment is now published!</h1>'
+            html: htmlString
         },
         (err, info) => {
             if (err) {
