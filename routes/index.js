e577105c320d09ed6d1088586c4bde8eb66f6e84
zuvvil <zuvvilgautam007@gmail.com>
2023-10-10 10:59:13 +0530

    Actions and Routes::Likes

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ router.use('/user', require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments', require('./comments'));
 
+router.use('/likes', require('./likes'));
+
 router.use('/api', require('./api'));
 
 // for any further routes, access from here
