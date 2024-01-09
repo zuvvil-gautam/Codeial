84210d23c53eec6359a40dbd9506e9a9e61e3379
zuvvil <zuvvilgautam007@gmail.com>
2024-01-09 22:20:55 +0530

    ChatBox: Send and Receive Messages

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -15,6 +15,10 @@ module.exports.chatSockets = function(socketServer){
                 socket.join(data.chatroom);
 
                 io.in(data.chatroom).emit('user_joined',data);
-            })
+            });
+
+            socket.on('send_message',function(data){
+                io.in(data.chatroom).emit('receive_message',data);
+            });
         });
 }
