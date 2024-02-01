ce6cab994b752059913e10496ccb732b36b503a8
zuvvil <zuvvilgautam007@gmail.com>
2024-02-01 14:04:49 +0530

    Reset Forgot Password using email

--- controller/forgotPassword_controller.js
+++ controller/forgotPassword_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const ForgotPswd = require('../models/forgotPassword');
+const nodeMailer = require('../config/nodemailer');
 
 module.exports.forgot = function (request, respond) {
     // return respond.end('<h1>forgot password</h1>');
@@ -40,14 +41,24 @@ module.exports.setEmail = async function (request, respond) {
 
         await forgotPswdToken.save();
 
-        // ---------- Send the reset password link to the user's email (you can implement this part)
+        const mailOptions = {
+            from: 'simran2mandal@gmail.com',
+            to: email,
+            subject: 'Reset Password Link',
+            html: `<p>Click <a href="http://localhost:5000/forgotPswd/reset-password/${accessToken}" >here</a> to reset your password. </p>`
+        };
 
-        // return respond.render('forgot_check-email', { 
-        //     email: email,
-        //     title: "forgot_check-email"
-        // });  -----------
+        nodeMailer.transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Email sent: ', info.respond);
+            }
+        });
 
-        return respond.render('forgot_change-password', {
+        console.log('email --->', email);
+
+        return respond.render('forgot_check-email', {
             title: 'Codeial | change password',
             token: accessToken,
             email: email
@@ -63,6 +74,7 @@ module.exports.setEmail = async function (request, respond) {
 
 module.exports.getResetPassword = async function (request, respond) {
     try {
+        console.log('getReset Link--->', request);
         const token = request.params.token;
         console.log('token', token);
         // Find the forgot password document with the provided token
@@ -74,10 +86,16 @@ module.exports.getResetPassword = async function (request, respond) {
             });
         }
 
+        const user = await User.findById(forgotPswdToken.user);
+        if (!user) {
+            return respond.status(404).send('User not found');
+        }
+
 
         return respond.render('forgot_change-password', {
             title: 'Codeial | change password',
-            token: token
+            token: token,
+            email: user.email
         });
     } catch (err) {
         console.log('Internal server error: ', err);
