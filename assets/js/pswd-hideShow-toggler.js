0bfd047c589317d1f37ede2a70528b8b6a485f58
zuvvil <zuvvilgautam007@gmail.com>
2024-02-01 13:51:05 +0530

    Add Feature:: Toggle(show/hide) password

---
+++ assets/js/pswd-hideShow-toggler.js
@@ -0,0 +1,14 @@
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const icon = input.nextElementSibling.querySelector('i');
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    } else {
+        input.type = "password";
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    }
+}
