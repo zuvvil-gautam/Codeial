7c66c4381afc2dbd3865e22997c1cbf04d916c7a
zuvvil <zuvvilgautam007@gmail.com>
2024-02-01 12:42:20 +0530

    Add Schema:Chats

---
+++ models/chat.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+});
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+module.exports = Chat;
